b714864a1707c42d6ab723572cfd8b245256a1b9
descholar-ceo <emmanuellamugi@gmail.com>
2021-06-19 13:48:55 +0200

    Edit origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'https://opportunity-feed.netlify.app/']
+    origins 'https://opportunity-feed.netlify.app'
 
     resource '*',
       headers: :any,
